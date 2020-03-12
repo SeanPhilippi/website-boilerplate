bd287e7a24beac96b05ad5a599079103312cb66b
Sean Philippi <s.philippi@rsitex.com>
2020-03-11 20:30:29 -0500

    some fixes, email still 404ing

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,7 +1,6 @@
 <?php
 // Check for empty fields
-if(empty($_POST['firstName']) ||
-   empty($_POST['lastName'])  ||
+if(empty($_POST['name']) ||
    empty($_POST['email'])     ||
    empty($_POST['location'])  ||
    empty($_POST['phone'])     ||
@@ -14,13 +13,14 @@ if(empty($_POST['firstName']) ||
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
+$location = strip_tags(htmlspecialchars($_POST['location']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
 $to = 'hadyn.inniss1@qubtx.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "QUBTX.com Contact Form:  $firstName $lastName";
-$email_body = "You have received a new message from your QUBTX.com contact form.\n\n"."Here are the details:\n\nName: $firstName $lastName\n\nEmail: $email_address\n\nLocation: $location\n\nPhone: $phone\n\nMessage:\n$message";
+$email_subject = "QUBTX.com Contact Form:  $name";
+$email_body = "You have received a new message from your QUBTX.com contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nLocation: $location\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@qubtx.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
