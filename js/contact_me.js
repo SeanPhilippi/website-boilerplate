28f96be8014fa5f4c3b8d4b397205e496c2803a0
Sean Philippi <s.philippi@rsitex.com>
2020-03-15 13:07:57 -0500

    shifted back to contact_me.js, validation errors working, 200 status code, no email

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,23 +1,33 @@
 $(function() {
+  // get values from FORM
+  const firstName = $("input#firstName").val();
+  const lastName = $("input#lastName").val();
+  const email = $("input#email").val();
+  const location = $("input#location").val();
+  const phone = $("input#phone").val();
+  const message = $("textarea#message").val();
 
   $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
     preventSubmit: true,
     submitError: function($form, event, errors) {
       // additional error messages or events
+      $('#success').html("<div class='alert alert-danger col-md-12'>");
+      $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+        .append("</button>");
+      $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that our mail server is not responding. Please try again later!"));
+      $('#success > .alert-danger').append('</div>');
+      //clear all fields
+      $('#contactForm').trigger("reset");
     },
     submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
-      // get values from FORM
-      var firstName = $("input#firstName").val();
-      var lastName = $("input#lastName").val();
-      var email = $("input#email").val();
-      var location = $("input#location").val();
-      var phone = $("input#phone").val();
-      var message = $("textarea#message").val();
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
+        url: "https://formspree.io/maybqnpy",
+        headers: {  'Access-Control-Allow-Origin': 'https://formspree.io/maybqnpy' },
+        dataType: "json",
+        crossDomain: true,
         type: "POST",
         data: {
           name: `${firstName} ${lastName}`,
@@ -27,35 +37,46 @@ $(function() {
           message: message
         },
         cache: false,
-        success: function() {
-          // Success message
-          $('#success').html("<div class='alert alert-success'>");
-          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
-          $('#success > .alert-success')
-            .append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        },
-        error: function(xhr, status, error) {
-            console.log('error', error)
-          // Fail message
-          $('#success').html("<div class='alert alert-danger col-md-12'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that our mail server is not responding. Please try again later!"));
-          $('#success > .alert-danger').append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        },
+        // success: function() {
+        //   // Success message
+        //   $('#success').html("<div class='alert alert-success'>");
+        //   $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+        //     .append("</button>");
+        //   $('#success > .alert-success')
+        //     .append("<strong>Your message has been sent. </strong>");
+        //   $('#success > .alert-success')
+        //     .append('</div>');
+        //   //clear all fields
+        //   $('#contactForm').trigger("reset");
+        // },
+        // error: function(xhr, status, error) {
+        //     console.log('error', error)
+        //     console.log('status', status)
+        //     console.log('xhr', xhr)
+        //   // Fail message
+        //   $('#success').html("<div class='alert alert-danger col-md-12'>");
+        //   $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+        //     .append("</button>");
+        //   $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that our mail server is not responding. Please try again later!"));
+        //   $('#success > .alert-danger').append('</div>');
+        //   //clear all fields
+        //   $('#contactForm').trigger("reset");
+        // },
         complete: function() {
           setTimeout(function() {
             $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
           }, 1000);
         }
       });
+      $('#success').html("<div class='alert alert-success'>");
+      $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+        .append("</button>");
+      $('#success > .alert-success')
+        .append("<strong>Your message has been sent. </strong>");
+      $('#success > .alert-success')
+        .append('</div>');
+      //clear all fields
+      $('#contactForm').trigger("reset");
     },
     filter: function() {
       return $(this).is(":visible");
